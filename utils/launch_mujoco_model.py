e11f63d619c2426239022016a29e35aacba724b8
stevenwman <sman2@andrew.cmu.edu>
2025-04-10 22:43:13 -0400

    mugatu foot reposition again

--- utils/launch_mujoco_model.py
+++ utils/launch_mujoco_model.py
@@ -7,13 +7,13 @@ import mujoco.viewer
 # file_path = "robots/duplo_ballfeet_mjcf/scene_motor_temp.xml"
 # file_path = os.path.join(curr_dir, file_rel_path)
 # file_path = "robots/zippy_mjcf/scene_motor_scaled_temp.xml"
-file_path = "old/old_robot_files/mugatu_nice_feet_urdf/robot.urdf"
+file_path = "old/old_robot_files/mugatu_nice_feet_fixed_urdf/robot.urdf"
 
 # Load your model
 model = mujoco.MjModel.from_xml_path(file_path)
 # Create a simulation data structure
 data = mujoco.MjData(model)
-mujoco.mj_saveLastXML("old/old_robot_files/mugatu_nice_feet_urdf/robot.xml", model)
+mujoco.mj_saveLastXML("old/old_robot_files/mugatu_nice_feet_fixed_urdf/robot.xml", model)
 
 # Launch the viewer (GUI)
 mujoco.viewer.launch(model, data)
